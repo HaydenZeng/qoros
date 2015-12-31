8e7f10001dcf27ba602ec0ff215bff1f8771bf51
bzhang <zhangyuxiaolove@126.com>
2015-12-31 15:36:13 +0800

    bug

--- server/config/autoload/wechat.global.php
+++ server/config/autoload/wechat.global.php
@@ -1,12 +1,41 @@
 <?php
 
 return array(
+//    'wechat' => array(
+//        'url' => 'http://qoros.jiubei.me/wechat',
+//        'token' => 'qoros',
+//        'EncodingAESKey' => 'hBNVdWU5i56ZqcsH38bwRrDDdALsljavwixhIILDXRi',
+//        'appid' => 'wx95ab641d29e1e251',
+//        'secret' => 'a3eb94bd006dc9122601824262ffcebf',
+//        // 菜单配置
+//        'menu' => array(
+//            array('name' => '车型展示', 'type' => 'view',  'sub_button' => array(
+//                array('name' => '观致逸云', 'type' => 'view', 'url' => 'http://www.qoros.com/qoros_qloud/?utm_medium=wechatr'),
+//                array('name' => '观致3轿车', 'type' => 'view', 'url' => 'http://www.qoros.com/qoros_3_sedan/?utm_medium=wechat'),
+//                array('name' => '观致3五门版', 'type' => 'view', 'url' => 'http://www.qoros.com/qoros_3_hatch/?utm_medium=wechat'),
+//                array('name' => '观致3都市SUV', 'type' => 'view', 'url' => 'http://www.qoros.com/qoros_3_city_suv/?utm_medium=wechat'),
+//                array('name' => '观致5 SUV', 'type' => 'view', 'url' => 'http://www.qoros.com/qoros_5_suv/?utm_source=officialqorosauto&utm_medium=wechat'),
+//            )),
+//            array('name' => '素朴生活', 'sub_button' => array(
+//                array('name' => '保险计划', 'type' => 'view', 'url' => 'http://www.qoros.com/insurance/?utm_medium=wechat'),
+//                array('name' => '观致超惠车贷专案', 'type' => 'view', 'url' => 'http://www.qoros.com/financing/?utm_medium=wechat'),
+//                array('name' => '查找经销商', 'type' => 'view', 'url' => 'http://www.qoros.com/dealers/?utm_medium=wechat'),
+//                array('name' => '预约试驾', 'type' => 'view', 'url' => 'http://www.qoros.com/test_drive/?utm_medium=wechat'),
+//            )),
+//            array('name' => '最新活动', 'sub_button' => array(
+//                array('name' => '观致5-暖心计划', 'type' => 'view', 'url' => 'http://qwechat.chinacloudapp
+//                .cn/qoros/user/register'),
+//                array('name' => '观致5-任性计划', 'type' => 'view', 'url' => 'http://qoroswechat.chinacloudapp.cn/frontend/web/index.php?r=site/order'),
+//            )),
+//        ),
+//    ),
+
     'wechat' => array(
-        'url' => 'http://qoros.jiubei.me/wechat',
+        'url' => 'http://qoroswechat.chinacloudapp.cn/qoros/wechat',
         'token' => 'qoros',
-        'EncodingAESKey' => 'hBNVdWU5i56ZqcsH38bwRrDDdALsljavwixhIILDXRi',
-        'appid' => 'wx95ab641d29e1e251',
-        'secret' => 'a3eb94bd006dc9122601824262ffcebf',
+        'EncodingAESKey' => 'HsjLzQpjG3v3HmLMTkjkVilOJ5A38rO7rqdTS7kcIp2',
+        'appid' => 'wxe1e68f1d9cfd957e',
+        'secret' => '532d06f7d5879700b810a8cfd2ba7710',
         // 菜单配置
         'menu' => array(
             array('name' => '车型展示', 'type' => 'view',  'sub_button' => array(
@@ -23,7 +52,7 @@ return array(
                 array('name' => '预约试驾', 'type' => 'view', 'url' => 'http://www.qoros.com/test_drive/?utm_medium=wechat'),
             )),
             array('name' => '最新活动', 'sub_button' => array(
-                array('name' => '观致5-暖心计划', 'type' => 'view', 'url' => 'http://qwechat.chinacloudapp
+                array('name' => '观致5-暖心计划', 'type' => 'view', 'url' => 'http://qoroswechat.chinacloudapp
                 .cn/qoros/user/register'),
                 array('name' => '观致5-任性计划', 'type' => 'view', 'url' => 'http://qoroswechat.chinacloudapp.cn/frontend/web/index.php?r=site/order'),
             )),
--- server/module/Wechat/src/Wechat/Entity/WechatMessageEntity.php
+++ server/module/Wechat/src/Wechat/Entity/WechatMessageEntity.php
@@ -15,7 +15,10 @@ class WechatMessageEntity extends Entity
     const  TYPE_LUCKY_DRAW = 'lucky_draw';
     const  TYPE_FINISH_SHARE = 'share';
 
-    const TEMPLATE_LUCKY_DRAW = 'xA0zgdKmPFlMQHOXZIm_d5eDejK0U2-po7knuB6CVkM';
-    const TEMPLATE_FINISH_SHARE = '5TW43udgbwFURcddwWECWmGFO4n2lgUiG0cmihXEirs';
+//    const TEMPLATE_LUCKY_DRAW = 'xA0zgdKmPFlMQHOXZIm_d5eDejK0U2-po7knuB6CVkM';
+//    const TEMPLATE_FINISH_SHARE = '5TW43udgbwFURcddwWECWmGFO4n2lgUiG0cmihXEirs';
+
+    const TEMPLATE_LUCKY_DRAW = 'oknrrK8hBEGhu6EsgJFvcwmAllthusyqfX9xJOXtQJc';
+    const TEMPLATE_FINISH_SHARE = '_nqcKbUHEg0QGpmLtoXER24rB4q9YUILUm5ukzV9yM8';
 
 }
