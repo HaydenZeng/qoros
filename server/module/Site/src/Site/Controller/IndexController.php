149a9446777ff8d86a5a3bba56d89b081d9dceca
bzhang <zhangyuxiaolove@126.com>
2015-12-31 14:42:57 +0800

    bug

--- server/module/Site/src/Site/Controller/IndexController.php
+++ server/module/Site/src/Site/Controller/IndexController.php
@@ -15,6 +15,7 @@ use Site\Entity\UserEntity;
 use Site\Model\Activity;
 use Site\Model\District;
 use Site\Model\Share;
+use Wechat\Entity\WechatMessageEntity;
 use Wechat\Model\Wechat;
 use Zend\View\Model\ViewModel;
 use Site\Model\User;
@@ -194,7 +195,16 @@ class IndexController extends Controller{
 
         if(!$this->share->getByActivityIdAndUserId($activityId,$user->id)){
             $this->share->save($share);
+
+            //send share message
+            $data = array('first'=>'活动分享通知','keyword1'=>'暖心活动分享','keyword2'=>date("Y-m-d H:i:s"),'keyword2'=>'完成',
+                'remark'=>'敬爱的车主，恭喜您成功完成“暖心计划”首个神秘任务！我们将会为您寄出观致汽车暖心礼包。为便于礼物顺利到达您的手中，请务必核对手机首次注册时所填写的地址是否正确。如有变动，可联系观致小Q进行更改。再次感谢您对观致汽车“暖心计划”的支持，祝您生活愉快。');
+            $tplId = WechatMessageEntity::TEMPLATE_FINISH_SHARE;
+            $this->wechat->sendTemplateMessage($user->open_id, $tplId, $data);
         }
+
+
+
         return new UnifyJsonModel();
     }
 
--- server/module/Wechat/src/Wechat/Entity/WechatMessageEntity.php
+++ server/module/Wechat/src/Wechat/Entity/WechatMessageEntity.php
@@ -13,6 +13,9 @@ class WechatMessageEntity extends Entity
     const ALIAS = 'wcmessage';
 
     const  TYPE_LUCKY_DRAW = 'lucky_draw';
+    const  TYPE_FINISH_SHARE = 'share';
 
     const TEMPLATE_LUCKY_DRAW = 'xA0zgdKmPFlMQHOXZIm_d5eDejK0U2-po7knuB6CVkM';
+    const TEMPLATE_FINISH_SHARE = '5TW43udgbwFURcddwWECWmGFO4n2lgUiG0cmihXEirs';
+
 }
