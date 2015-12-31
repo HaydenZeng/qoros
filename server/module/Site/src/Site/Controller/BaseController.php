5d2413d47f4551c4139a48ab61378e3887561564
bzhang <zhangyuxiaolove@126.com>
2015-12-31 11:30:35 +0800

    bug

--- server/module/Site/src/Site/Controller/BaseController.php
+++ server/module/Site/src/Site/Controller/BaseController.php
@@ -20,7 +20,7 @@ class BaseController extends Controller {
                 $code = $this->getParam('code', false);
                 if(!$code){
                     $redirectUrl = $this->wechat->getOauthRedirect('http://'.$_SERVER['SERVER_NAME']
-                        .'/qoros/user/register?redirect='.$redirect);
+                        .'/qoros/user/agreement?redirect='.$redirect);
                     return $this->redirect()->toUrl($redirectUrl);
                 }
                 $tokenData = $this->wechat->getOauthData();
