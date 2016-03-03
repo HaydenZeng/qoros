30da1ff313759e20a0882b8c3bd69393a4d1f269
bzhang <baozhu@dang5.com>
2016-03-03 12:20:24 +0800

    bug

--- server/config/autoload/wechat.global.php
+++ server/config/autoload/wechat.global.php
@@ -1,40 +1,13 @@
 <?php
 
 return array(
-//    'wechat' => array(
-//        'url' => 'http://test_qoros.jiubei.me/test_qoros/wechat',
-//        'token' => 'qoros',
-//        'EncodingAESKey' => 'hBNVdWU5i56ZqcsH38bwRrDDdALsljavwixhIILDXRi',
-//        'appid' => 'wx95ab641d29e1e251',
-//        'secret' => 'a3eb94bd006dc9122601824262ffcebf',
-//        // 菜单配置
-//        'menu' => array(
-//            array('name' => '车型展示', 'type' => 'view',  'sub_button' => array(
-//                array('name' => '观致逸云', 'type' => 'view', 'url' => 'http://www.qoros.com/qoros_qloud/?utm_medium=wechatr'),
-//                array('name' => '观致3轿车', 'type' => 'view', 'url' => 'http://www.qoros.com/qoros_3_sedan/?utm_medium=wechat'),
-//                array('name' => '观致3五门版', 'type' => 'view', 'url' => 'http://www.qoros.com/qoros_3_hatch/?utm_medium=wechat'),
-//                array('name' => '观致3都市SUV', 'type' => 'view', 'url' => 'http://www.qoros.com/qoros_3_city_suv/?utm_medium=wechat'),
-//                array('name' => '观致5 SUV', 'type' => 'view', 'url' => 'http://www.qoros.com/qoros_5_suv/?utm_source=officialqorosauto&utm_medium=wechat'),
-//            )),
-//            array('name' => '购车助手', 'sub_button' => array(
-//                array('name' => '保险计划', 'type' => 'view', 'url' => 'http://www.qoros.com/insurance/?utm_medium=wechat'),
-//                array('name' => '观致超惠车贷专案', 'type' => 'view', 'url' => 'http://www.qoros.com/financing/?utm_medium=wechat'),
-//                array('name' => '查找经销商', 'type' => 'view', 'url' => 'http://www.qoros.com/dealers/?utm_medium=wechat'),
-//                array('name' => '预约试驾', 'type' => 'view', 'url' => 'http://www.qoros.com/test_drive/?utm_medium=wechat'),
-//            )),
-//            array('name' => '最新活动', 'sub_button' => array(
-//                array('name' => '观致5-暖心计划', 'type' => 'view', 'url' => 'http://qoroswechat.chinacloudapp.cn/test_qoros/user/register'),
-//                array('name' => '观致5-任性计划', 'type' => 'view', 'url' => 'http://qoroswechat.chinacloudapp.cn/frontend/web/index.php?r=site/order'),
-//            )),
-//        ),
-//    ),
-
+//这里是测试环境的配置
     'wechat' => array(
-        'url' => 'http://qoroswechat.chinacloudapp.cn/test_qoros/wechat',
+        'url' => 'http://test_qoros.jiubei.me/test_qoros/wechat',
         'token' => 'qoros',
-        'EncodingAESKey' => 'HsjLzQpjG3v3HmLMTkjkVilOJ5A38rO7rqdTS7kcIp2',
-        'appid' => 'wxe1e68f1d9cfd957e',
-        'secret' => '532d06f7d5879700b810a8cfd2ba7710',
+        'EncodingAESKey' => 'hBNVdWU5i56ZqcsH38bwRrDDdALsljavwixhIILDXRi',
+        'appid' => 'wx95ab641d29e1e251',
+        'secret' => 'a3eb94bd006dc9122601824262ffcebf',
         // 菜单配置
         'menu' => array(
             array('name' => '车型展示', 'type' => 'view',  'sub_button' => array(
@@ -56,6 +29,35 @@ return array(
             )),
         ),
     ),
+
+//这里是线上环境的配置
+//    'wechat' => array(
+//        'url' => 'http://qoroswechat.chinacloudapp.cn/test_qoros/wechat',
+//        'token' => 'qoros',
+//        'EncodingAESKey' => 'HsjLzQpjG3v3HmLMTkjkVilOJ5A38rO7rqdTS7kcIp2',
+//        'appid' => 'wxe1e68f1d9cfd957e',
+//        'secret' => '532d06f7d5879700b810a8cfd2ba7710',
+//        // 菜单配置
+//        'menu' => array(
+//            array('name' => '车型展示', 'type' => 'view',  'sub_button' => array(
+//                array('name' => '观致逸云', 'type' => 'view', 'url' => 'http://www.qoros.com/qoros_qloud/?utm_medium=wechatr'),
+//                array('name' => '观致3轿车', 'type' => 'view', 'url' => 'http://www.qoros.com/qoros_3_sedan/?utm_medium=wechat'),
+//                array('name' => '观致3五门版', 'type' => 'view', 'url' => 'http://www.qoros.com/qoros_3_hatch/?utm_medium=wechat'),
+//                array('name' => '观致3都市SUV', 'type' => 'view', 'url' => 'http://www.qoros.com/qoros_3_city_suv/?utm_medium=wechat'),
+//                array('name' => '观致5 SUV', 'type' => 'view', 'url' => 'http://www.qoros.com/qoros_5_suv/?utm_source=officialqorosauto&utm_medium=wechat'),
+//            )),
+//            array('name' => '购车助手', 'sub_button' => array(
+//                array('name' => '保险计划', 'type' => 'view', 'url' => 'http://www.qoros.com/insurance/?utm_medium=wechat'),
+//                array('name' => '观致超惠车贷专案', 'type' => 'view', 'url' => 'http://www.qoros.com/financing/?utm_medium=wechat'),
+//                array('name' => '查找经销商', 'type' => 'view', 'url' => 'http://www.qoros.com/dealers/?utm_medium=wechat'),
+//                array('name' => '预约试驾', 'type' => 'view', 'url' => 'http://www.qoros.com/test_drive/?utm_medium=wechat'),
+//            )),
+//            array('name' => '最新活动', 'sub_button' => array(
+//                array('name' => '观致5-暖心计划', 'type' => 'view', 'url' => 'http://qoroswechat.chinacloudapp.cn/test_qoros/user/register'),
+//                array('name' => '观致5-任性计划', 'type' => 'view', 'url' => 'http://qoroswechat.chinacloudapp.cn/frontend/web/index.php?r=site/order'),
+//            )),
+//        ),
+//    ),
 
 
 );
